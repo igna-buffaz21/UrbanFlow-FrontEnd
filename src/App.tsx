dde043dce95193db684e254162d6518b7188d2f3
Ignacio <ignaciobuffaz73@gmail.com>
2026-05-12 11:45:19 -0300

    firts commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,57 @@
-import { useState } from 'react'
-import './App.css'
+import {
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+  useUser,
+  useAuth,
+} from "@clerk/react";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { user, isLoaded } = useUser();
+  const { getToken, isSignedIn } = useAuth();
+
+  useEffect(() => {
+    async function showUserData() {
+      if (!isLoaded) return;
+
+      console.log("USER COMPLETO:", user);
+
+      if (!user || !isSignedIn) {
+        console.log("No hay usuario logueado");
+        return;
+      }
+
+      const token = await getToken();
+
+      console.log("TOKEN:", token);
+
+      console.log("ID:", user.id);
+      console.log("Email:", user.primaryEmailAddress?.emailAddress);
+      console.log("Nombre:", user.firstName);
+      console.log("Apellido:", user.lastName);
+      console.log("Imagen:", user.imageUrl);
+    }
+
+    showUserData();
+  }, [user, isLoaded, isSignedIn, getToken]);
 
   return (
-    <>
-  <h1>hello world</h1>
-    </>
-  )
+    <div>
+      <h1>Mi app</h1>
+
+      <Show when="signed-out">
+        <SignInButton mode="modal" />
+        <SignUpButton mode="modal" />
+      </Show>
+
+      <Show when="signed-in">
+        <p>Bienvenido</p>
+        <UserButton />
+      </Show>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ClerkProvider } from "@clerk/react";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!publishableKey) {
+  throw new Error("Falta VITE_CLERK_PUBLISHABLE_KEY en el .env");
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={publishableKey}>
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>
+);
